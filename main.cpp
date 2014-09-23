24778e25a95af530ab53c94277cf2efb18ddc6c3
Scott Olson <scott@scott-olson.org>
2014-09-22 22:38:20 -0400

    Add the rest of the boilerplate.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
+#include <cstdio>
+#include <GL/glew.h>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 
 int main() {
-  sf::Window w(sf::VideoMode(1366, 768), "gg", sf::Style::Fullscreen);
-  sf::sleep(sf::seconds(1.f));
+  sf::Window window(sf::VideoMode(800, 600), "gg", sf::Style::Close);
+  glewExperimental = GL_TRUE;
+  glewInit();
+
+  GLuint vertexBuffer;
+  glGenBuffers(1, &vertexBuffer);
+  printf("%u\n", vertexBuffer);
+
+  bool running = true;
+  while (running) {
+    sf::Event windowEvent;
+    while (window.pollEvent(windowEvent)) {
+      switch (windowEvent.type) {
+        case sf::Event::Closed:
+          running = false;
+          break;
+        case sf::Event::KeyPressed:
+          if (windowEvent.key.code == sf::Keyboard::Escape)
+            running = false;
+          break;
+      }
+    }
+  }
   return 0;
 }

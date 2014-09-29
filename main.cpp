132360fee4c461122209a27fcf360ec6451c758e
Scott Olson <scott@scott-olson.org>
2014-09-29 13:49:33 -0400

    Switch to element buffer drawing.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>
+#include <iostream>
 #include <GL/glew.h>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
@@ -39,8 +39,8 @@ GLuint compileShader(GLenum shaderType, const char* shaderSource) {
   if (!status) {
     char buffer[512];
     glGetShaderInfoLog(shader, 512, NULL, buffer);
-    puts(buffer);
-    abort();
+    std::cerr << buffer << '\n';
+    std::abort();
   } else {
     return shader;
   }
@@ -51,8 +51,7 @@ int main() {
   settings.depthBits = 24;
   settings.stencilBits = 8;
 
-  sf::Window window(sf::VideoMode(800, 600, 32),
-                    "gg",
+  sf::Window window(sf::VideoMode(800, 600, 32), "gg",
                     sf::Style::Titlebar | sf::Style::Close, settings);
 
   // Initialize GLEW.
@@ -65,17 +64,26 @@ int main() {
   glBindVertexArray(vao);
 
   // Create a Vertex Buffer Object and copy the vertex data to it.
+  GLfloat vertices[] = {
+    -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
+     0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
+     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
+    -0.5f, -0.5f, 1.0f, 0.0f, 1.0f, // Bottom-left
+  };
   GLuint vbo;
   glGenBuffers(1, &vbo);
-
-  GLfloat vertices[] = {
-    0.0f,  0.5f, 1.0f, 0.0f, 0.0f,
-    0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-    -0.5f, -0.5f, 0.0f, 0.0f, 1.0f
-  };
-
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  GLuint elements[] = {
+    0, 1, 2,
+    2, 3, 0
+  };
+  GLuint ebo;
+  glGenBuffers(1, &ebo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements,
+               GL_STATIC_DRAW);
 
   // Create and compile the vertex and fragment shaders.
   GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexSource);
@@ -110,7 +118,17 @@ int main() {
         case sf::Event::Closed:
           window.close();
           break;
+        case sf::Event::KeyPressed:
+          if (windowEvent.key.code == sf::Keyboard::Escape)
+            window.close();
+          break;
+        default:
+          break;
       }
+    }
+
+    if (!window.isOpen()) {
+      break;
     }
 
     // Clear the screen to black.
@@ -120,19 +138,11 @@ int main() {
     // Draw a triangle from the 3 vertices.
     float time = (float)clock() / (float)CLOCKS_PER_SEC;
     glUniform3f(uniColor, (sin(time * 4.0f) + 1.0f) / 2.0f, 0.0f, 0.0f);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     // Swap buffers.
     window.display();
   }
-
-  glDeleteProgram(shaderProgram);
-  glDeleteShader(fragmentShader);
-  glDeleteShader(vertexShader);
-
-  glDeleteBuffers(1, &vbo);
-
-  glDeleteVertexArrays(1, &vao);
 
   return 0;
 }
